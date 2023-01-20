b9c17caf17471d46d164ffe4f21e330a84eb67d3
Jörg-Christian Müller <jcm@shv-is.eu>
2023-01-21 00:09:14 +0100

    Lesson 97: web-app - final running version

--- web-app/src/main/java/guru/springframework/controllers/UserController.java
+++ web-app/src/main/java/guru/springframework/controllers/UserController.java
@@ -1,5 +1,6 @@
 package guru.springframework.controllers;
 
+import guru.springframework.converters.UserMapper;
 import guru.springframework.domain.UserCommand;
 import guru.springframework.entities.User;
 
@@ -9,9 +10,6 @@ import guru.springframework.entities.User;
 public class UserController {
 
     User saveUser(UserCommand command) {
-
-        // fake impl
-        return new User();
+        return UserMapper.INSTANCE.userCommandToUser(command);
     }
-
 }
